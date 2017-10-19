bd835a36f5a14e0df83423edd05f1deb1f4ba0c3
Manuel Boesch <Boesch@RPCBEBO.local>
2017-10-19 09:40:54 +0200

    First commit; setup stuff

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Threading;
+
+namespace TaskbarHide
+{
+
+    public class Program
+    {
+        private const int SW_HIDE = 0;
+        private const int SW_SHOW = 5;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern System.IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr FindWindowEx(IntPtr parentHandle, IntPtr childAfter, string className, string windowTitle);
+        [DllImport("user32.dll")]
+        private static extern IntPtr FindWindowEx(IntPtr parentHwnd, IntPtr childAfterHwnd, IntPtr className, string windowText);
+        [DllImport("user32.dll")]
+        private static extern int ShowWindow(IntPtr hwnd, int nCmdShow);
+        private delegate bool EnumThreadProc(IntPtr hwnd, IntPtr lParam);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hiding taskbar. Press T to toggle visiblilty ...");
+            ConsoleKey key;
+            Boolean visible = false;
+            Boolean running = true;
+
+            while (running)
+            {
+                if (Console.KeyAvailable)
+                {
+                    key = Console.ReadKey(true).Key;
+                    switch (key)
+                    {
+                        case ConsoleKey.Escape:
+                            running = false;
+                            visible = true;
+                            break;
+                        case ConsoleKey.T:
+                            visible = !visible;
+                            break;
+                    }
+                }
+
+                ShowTaskbar(visible);
+            }
+
+        }
+
+        private static void ShowTaskbar(bool show)
+        {
+            // get taskbar window
+            IntPtr taskBarWnd = FindWindow("Shell_TrayWnd", null);
+
+            // try it the WinXP way first...
+            IntPtr startWnd = FindWindowEx(taskBarWnd, IntPtr.Zero, "Button", "Start");
+
+            if (startWnd == IntPtr.Zero)
+            {
+                // try an alternate way, as mentioned on CodeProject by Earl Waylon Flinn
+                startWnd = FindWindowEx(IntPtr.Zero, IntPtr.Zero, (IntPtr)0xC017, "Start");
+            }
+
+            if (startWnd == IntPtr.Zero)
+            {
+                // ok, let's try the Vista easy way...
+                startWnd = FindWindow("Button", null);
+            }
+
+            ShowWindow(taskBarWnd, show ? SW_SHOW : SW_HIDE);
+            ShowWindow(startWnd, show ? SW_SHOW : SW_HIDE);
+        }
+    }
+}
